a7500cb621e2f2cb277ebff1ad32b500da2dff1c
Bui Tuan Dung (TECHVIFY.G2) <dungbt@techvify.com.vn>
2022-05-06 14:07:37 +0700

    add taidlwind css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import { ChakraProvider } from '@chakra-ui/react';
-import { InboxScreen } from './InboxScreen';
-import { LoginScreen } from './LoginScreen';
-import { theme } from './theme';
-import { useAuth } from './useAuth';
+import { ChakraProvider } from "@chakra-ui/react";
+import { InboxScreen } from "./InboxScreen";
+import { LoginScreen } from "./LoginScreen";
+import { theme } from "./theme";
+import { useAuth } from "./useAuth";
 
 function App() {
   const [user, logIn] = useAuth();
 
   return (
     <ChakraProvider theme={theme}>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       {user?.token ? <InboxScreen /> : <LoginScreen onLogIn={logIn} />}
     </ChakraProvider>
   );
